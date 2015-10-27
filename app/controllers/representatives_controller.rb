162f604cde60d16d7117f9301a976463d3afa8f1
suhaibh <suhaib.habibullah@gmail.com>
2015-10-26 22:21:38 -0400

    Add rep title and portrait

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,7 +1,8 @@
 class RepresentativesController < ApplicationController
 
 	def reps
-		@reps = CLIENT.legislators_locate(params[:address])
+		# sets @reps to the results array
+		@reps = CLIENT.legislators_locate(params[:address]).results
 	end
 
 end
--- app/helpers/representatives_helper.rb
+++ app/helpers/representatives_helper.rb
@@ -1,2 +1,13 @@
 module RepresentativesHelper
+	def city_state
+		params[:address]
+	end
+
+	def full_title(rep)
+		"#{rep.title}. #{rep.first_name} #{rep.last_name} (#{rep.party})"
+	end
+
+	def portrait(rep)
+		"https://theunitedstates.io/images/congress/225x275/#{rep.bioguide_id}.jpg"
+	end
 end
