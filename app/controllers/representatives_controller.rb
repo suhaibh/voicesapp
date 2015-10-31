1a8cb328d8f4db543f907d8a2568a24eed8c3334
suhaibh <suhaib.habibullah@gmail.com>
2015-10-31 17:39:46 -0400

    Add dynamic twitter links

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -3,6 +3,7 @@ class RepresentativesController < ApplicationController
 	def reps
 		# sets @reps to the results array
 		@reps = CLIENT.legislators_locate(params[:address]).results
+		@rep = CLIENT.legislators_locate(params[:address])
 	end
 
 end
--- app/helpers/representatives_helper.rb
+++ app/helpers/representatives_helper.rb
@@ -10,4 +10,13 @@ module RepresentativesHelper
 	def portrait(rep)
 		"https://theunitedstates.io/images/congress/225x275/#{rep.bioguide_id}.jpg"
 	end
+
+	def rep_twitter(rep)
+		if rep.twitter_id
+			link_to("https://www.twitter.com/#{rep.twitter_id}", target: "_blank", class: "contact-link") do
+				'<i class="fa fa-twitter fa-lg rep-twitter"></i>'.html_safe
+			end
+		end
+	end
+
 end
