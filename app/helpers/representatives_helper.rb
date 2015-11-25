7a597dda13dd940637692b95d5cb64f45cfbfe5a
suhaibh <suhaib.habibullah@gmail.com>
2015-11-24 21:17:50 -0500

    Add email and facebook links

--- app/helpers/representatives_helper.rb
+++ app/helpers/representatives_helper.rb
@@ -11,11 +11,25 @@ module RepresentativesHelper
 		"https://theunitedstates.io/images/congress/225x275/#{rep.bioguide_id}.jpg"
 	end
 
+	def rep_facebook(rep)
+		if rep.facebook_id
+			link_to("https://www.facebook.com/#{rep.facebook_id}") do
+				content_tag(:i, "", class: "fa fa-facebook-official fa-lg rep-facebook contact-link")
+			end
+		end
+	end
+
 	def rep_twitter(rep)
 		if rep.twitter_id
 			link_to("https://www.twitter.com/#{rep.twitter_id}", target: "_blank", class: "contact-link") do
 				content_tag(:i, "", class: "fa fa-twitter fa-lg rep-twitter")
 			end
+		end
+	end
+
+	def rep_email(rep)
+		mail_to("#{rep.oc_email}") do 
+			content_tag(:i, "", class: "fa fa-envelope fa-lg contact-link")
 		end
 	end
 
