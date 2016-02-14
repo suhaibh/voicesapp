6b34aeaa90280db9719a81da75ae499fee1fb11a
suhaibh <suhaib.habibullah@gmail.com>
2016-02-14 15:35:07 -0500

    Remove experimental code

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -3,7 +3,7 @@ class RepresentativesController < ApplicationController
 	def reps
 		@address = "#{params[:city]} #{params[:state]}"
 		@reps = CLIENT.legislators_locate(@address).results
-		# @rep = CLIENT.legislators_locate(params[:address], params[:state])
+		# @rep = CLIENT.legislators_lcate(params[:address], params[:state])
 	end
 
 end
