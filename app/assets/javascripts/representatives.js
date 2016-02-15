57f28aa9babc9c837a89d134ee522bea5bb7b456
suhaibh <suhaib.habibullah@gmail.com>
2016-02-15 09:39:47 -0500

    Add Google Maps Geocode call to get lat/long to use for state legislators look up through sunlight api

--- app/assets/javascripts/representatives.js
+++ app/assets/javascripts/representatives.js
@@ -18,9 +18,22 @@ $(document).ready(function(){
 		}
 	}
 	
-	// make a call to google maps api to get longitutde and latitude based on city and state
+	// make a call to google maps api to get longitutde and latitude based on city and state then look up local reps through sunlight
 	$location = $('.city-state')
-	$.getJSON('http://openstates.org/api/v1/legislators/?state='+$location.data('state')+'&apikey=fc3288f6fc76432cab18b7a00cb4f3a9', function(data){
-		$stateLegislator = data})
+	$.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+$location.data("city")+$location.data("state")+'&key=AIzaSyCJjdXuDzjx5tOcYNHKuNQLAfeIya566pA', function(data){
+		$lat = data.results[0]['geometry']['location']['lat'].toFixed(2);
+		$lng = data.results[0]['geometry']['location']['lng'].toFixed(2);
+		$.getJSON('http://openstates.org/api/v1/legislators/geo/?lat='+$lat+'&long='+$lng+'&apikey=fc3288f6fc76432cab18b7a00cb4f3a9', function(data){
+		$stateLegislator = data;
+		displayStateReps($stateLegislator);
+		})
+		
+	})
+
+	// create method that displays state legislators information and call at the end of json method
+	displayStateReps = function(legislatorInfo){
+		$("<p>"+ legislatorInfo[0].last_name +"</p>").appendTo(".state-reps")
+	}
+
 });
 	
