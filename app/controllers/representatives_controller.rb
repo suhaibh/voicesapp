87246a21b37663d03deaaa760e2e6e81f7419194
suhaibh <suhaib.habibullah@gmail.com>
2016-02-16 23:58:26 -0500

    Add comments

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,8 +1,8 @@
 class RepresentativesController < ApplicationController
 
 	def reps
-		@address = "#{params[:city]} #{params[:state]}"
-		@reps = CLIENT.legislators_locate(@address).results
+		@address = "#{params[:city]} #{params[:state]}" 		# Formats params information in a way that can be used by Sunlight Foundation API
+		@reps = CLIENT.legislators_locate(@address).results		# Stores Sunlight API information in instance variable
 		# @rep = CLIENT.legislators_lcate(params[:address], params[:state])
 	end
 
