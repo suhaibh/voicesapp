cb3c62627445ae116f11c51efeed2efdfb1a6282
suhaibh <suhaib.habibullah@gmail.com>
2016-03-02 01:19:03 -0500

    Add js method to check for broken portrait links for state reps

--- app/assets/javascripts/representatives.js
+++ app/assets/javascripts/representatives.js
@@ -20,25 +20,8 @@ $(document).ready(function(){
 		}
 	}
 	
-	// make a call to google maps api to get longitutde and latitude based on city and state then look up local reps through sunlight
-	$location = $('.city-state')
-	$.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+$location.data("city")+$location.data("state")+'&key=AIzaSyCJjdXuDzjx5tOcYNHKuNQLAfeIya566pA', function(data){
-		$lat = data.results[0]['geometry']['location']['lat'].toFixed(2);
-		$lng = data.results[0]['geometry']['location']['lng'].toFixed(2);
-		$.getJSON('http://openstates.org/api/v1/legislators/geo/?lat='+$lat+'&long='+$lng+'&apikey=fc3288f6fc76432cab18b7a00cb4f3a9', function(data){
-		$stateLegislator = data;
-		// displayStateReps($stateLegislator); don't uncomment this until the function is done
-		})
-		
-	})
-
-	// create method that displays state legislators information and call at the end of json method, argument sould be $stateLegilator[i]
-	displayStateReps = function(legislatorInfo){
-		// create new div for individual rep and append it to the state reps div
-		$stateRepsDiv = $('<div>', {class: "col-xs-12 col-lg-4"}).appendTo($('.state-reps'));
-		$('.state-reps').append($stateRepsDiv);
-		$("<div>"+ legislatorInfo.last_name +"</div>").appendTo(".state-reps")
-	}
+	$imgHeight = $('.loaded').first().height();
+	$('.state-rep-portrait').height($imgHeight);
 
 });
 	
