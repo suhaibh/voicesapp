7f8b0da46d0164dd030e0e40b9b5ed0f8a4edbc6
suhaibh <suhaib.habibullah@gmail.com>
2016-03-02 01:20:32 -0500

    Add helper method do add placeholder portrait for broken state reps links

--- app/helpers/representatives_helper.rb
+++ app/helpers/representatives_helper.rb
@@ -54,8 +54,13 @@ module RepresentativesHelper
 	end
 
 	# create method for handling broken photo urls
-	def state_rep_portrait
-		
+	def state_rep_portrait state_rep
+		state_rep_url = HTTParty.get(state_rep["photo_url"])
+		if state_rep_url.code == 404
+			image_tag("profile_placeholder.png", class: "img-roudned state-rep-portrait")
+		else
+			image_tag(state_rep["photo_url"], class: "img-rounded rep-portrait loaded")
+		end
 	end
 
 end
